c9613dc93e78ff1ea10fcd3d6d794b764341f0ef
Igor <igor.korobov.gh@gmail.com>
2024-04-20 19:50:55 +0300

    Add Current() example

---
+++ examples/06-current.go
@@ -0,0 +1,24 @@
+package examples
+
+import (
+	"github.com/igorhub/devcard"
+)
+
+func DevcardCurrent(dc *devcard.Devcard) {
+	dc.SetTitle("Current devcard")
+
+	dc.Md("`devcard` package provides `Current` function, which returns the devcard currently being filled with cells.")
+
+	dc.Md("It point to the same devcard as the entry function parameter `dc`, as you can see:")
+
+	code(dc, `dc.Aval("dc == devcard.Current()", dc == devcard.Current())`)
+	dc.Aval("dc == devcard.Current()", dc == devcard.Current())
+
+	dc.Md("The reason it's exposed is to make it available to any function in our program, no matter how deeply nested. ",
+		"This can be useful for occasional printf-style debugging or for some quick experiments.")
+
+	todo()
+}
+
+func todo() {
+}
